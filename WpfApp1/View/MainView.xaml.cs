1f4f1e0345db88325da7d28c7e96798a94702f2a
Haqve_vu72 <Haqve_vu72@itstep.edu.az>
2023-07-12 19:03:23 +0400

    Complete

---
+++ WpfApp1/View/MainView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.ViewModel;
+
+namespace WpfApp1.View
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public ICommand _calculate { get; set; }
+        public MainView()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _calculate = new Calculate(Calculate,CanCalculate);
+        }
+        public void Calculate(object? parameter) 
+        {
+            int number_1 = int.Parse(first_number.Text.ToString());
+            int number_2 = int.Parse((second_number.Text.ToString()));
+            string selectedOperator = _operator.SelectedItem?.ToString();
+            if (selectedOperator=="+") 
+            {
+                _result.Text = (number_1+number_2).ToString();
+            }
+            else if (selectedOperator == "-")
+            {
+                _result.Text = (number_1 - number_2).ToString();
+            }
+            else if (selectedOperator == "*")
+            {
+                _result.Text = (number_1 * number_2).ToString();
+            }
+            else if (selectedOperator == "/")
+            {
+                _result.Text = (number_1 / number_2).ToString();
+            }
+        }
+        public bool CanCalculate(object? parameter) 
+        {
+            if (first_number.Text != "" && second_number.Text!="" && _operator.SelectedItem!=null) { return true; };
+            return false;
+        }
+    }
+}
---
+++ WpfApp1/ViewModel/Calculate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.ViewModel
+{
+    internal class Calculate : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public Calculate(Action<object?> execute, Predicate<object?> canExecute)
+        {
+            _execute = execute;
+            _canexecute = canExecute;
+        }
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?>? _canexecute;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canexecute == null || _canexecute.Invoke(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+    }
+}
+
